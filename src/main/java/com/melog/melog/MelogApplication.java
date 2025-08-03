1dbeade4a4dbc371506573a750da2af13ce1fbdb
박지훈 <jihoon@jihoon.local>
2025-08-03 23:20:39 +0900

    init : melog init

---
+++ src/main/java/com/melog/melog/MelogApplication.java
@@ -0,0 +1,13 @@
+package com.melog.melog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melog/melog/MelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melog.melog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
