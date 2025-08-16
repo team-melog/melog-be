0d9a0ee9627d53680947247eb19af989dffd294a
mohyeonMan <qkrwlgns0510@gmail.com>
2025-08-16 22:37:24 +0900

    feat : TTS 1차 개발 - 현재 emotion, emotionStrength 값 외부파라미터에 위임. - 감정과 수치로 TextToSpeakService 내부계산 하도록 수정 예정.

--- src/main/java/com/melog/melog/clova/config/ClovaStudioConfig.java
+++
@@ -1,8 +0,0 @@
-package com.melog.melog.clova.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ClovaStudioConfig {
-    // 공통 RestTemplateConfig에서 정의된 restTemplate 빈을 사용
-}
